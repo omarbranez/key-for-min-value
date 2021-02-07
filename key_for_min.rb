b22081ce7e122c19b3db3650975c2f5acb076ef2
Omar Branez <omar.branez@gmail.com>
2021-02-07 18:30:37 -0500

    commit tree 60741bd0c444fa41b9b7db6d7d6df9f88b649838

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,16 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
-
 def key_for_min_value(name_hash)
+    smallest_key, smallest_value = name_hash.first    
+    if name_hash.size == 0
+        return nil
+    else
+        name_hash.each do |key, value| 
+            if value < smallest_value
+                smallest_key = key
+        end
+    end
 
+    return smallest_key
+end
 end
